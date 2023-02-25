2b0d2eee0c1040845117825bea101acf0b3f4025
Syed Hussain Ather <shussainather@gmail.com>
2023-02-25 16:09:55 -0500

    can you train model

--- dcai1.py
+++ dcai1.py
@@ -26,3 +26,12 @@ def evaluate(clf):
 
 evaluate(sgd_clf)
 
+"""
+Can you train a more accurate model on the dataset (without changing the dataset)? You might find this scikit-learn classifier comparison handy, as well as the documentation for supervised learning in scikit-learn.
+
+One idea for a model you could try is a naive Bayes classifier.
+
+You could also try experimenting with different values of the model hyperparameters, perhaps tuning them via a grid search.
+
+Or you can even try training multiple different models and ensembling their predictions, a strategy often used to win prediction competitions like Kaggle.
+"""
