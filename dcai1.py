1ca756d5d916d9f459b1dbde321689a47e504148
Syed Hussain Ather <shussainather@gmail.com>
2023-02-25 16:08:41 -0500

    add pipeline

--- dcai1.py
+++ dcai1.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+
+sgd_clf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', SGDClassifier()),
+])
+
 train = pd.read_csv('reviews_train.csv')
 test = pd.read_csv('reviews_test.csv')
 
